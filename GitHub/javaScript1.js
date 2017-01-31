b0c52e70c53d577e7308e91e7f7c96d11e916d89
EderSando <edrob1987@gmail.com>
2017-01-31 10:34:05 -0600

    Add files via upload

---
+++ GitHub/javaScript1.js
@@ -0,0 +1,46 @@
+// Go to https://github.com/ricardo-hpe/js-training-hpe and review the content inside the README.md file.
+
+/* Exercises for lesson 1:
+1.- Write at least 10 lines of js code that prints out a phrase, it can be whatever you wanna print.
+2.- Review the length of at least 5 words/sentences.
+3.- Declare 5 different variables, it can be either a number or a String.
+4.- Ask a user 5 different questions.
+5.- Declare at least 5 arrays, either only numbers, Strings or mixed.*/
+
+// TIP: If you wanna review your results, go to http://www.webtoolkitonline.com/javascript-tester.html
+1.- Write at least 10 lines of js code that prints out a phrase, it can be whatever you wanna print.*/
+console.log("I would like to know you better please answer me the following questions");
+console.log("Please enter your answer when its requested?");
+console.log("All the information its confidential");
+console.log("All the information its use for bussiness reason");
+console.log("Please enter your answer when its requested?");
+console.log("What do you think about my JS Skills");
+console.log("Im just learning to program but it feels good to command the computer");
+console.log("Please enter your answer when its requested?");
+console.log("Im the HackerMan");
+console.log("Commanding in the computers");
+/*4.- Ask a user 5 different questions.*/
+/*3.- Declare 5 different variables, it can be either a number or a String.*/
+var fName = prompt ("Whats your First Name?");
+var lName = prompt ("Whats your Last name?");
+var age = prompt ("How old are you?");
+var location = prompt ("Where are you located?");
+var location = prompt ("Where do you work");
+var employeer = prompt ("Where do you work");
+
+/*2.- Review the length of at least 5 words/sentences.*/
+console.log(fName.length);
+console.log(lName.length);
+console.log(age.length);
+console.log(location.length);
+console.log(employeer.length);
+
+
+/*5.- Declare at least 5 arrays, either only numbers, Strings or mixed.*/
+var Narray = [100, 200, 300, 400, 500];
+var Larray = ["Ricardo", "Monica", "Javier", "Lucy", "Eduardo"];
+var MixArray =  ["Doom", 2016, "Left4Dead", 2014, "Minecraft", 2011];
+var SnArray = [250, 600, 900, 1200, 1500];
+var CrazyArray = ["Manager", "Brownies", "Tester", "Developer", "CrazyDot"];
+
+// TIP: If you wanna review your results, go to http://www.webtoolkitonline.com/javascript-tester.html
